6dfbd3c88585d7be6e6b05b0f85de2a22d790ca0
anikait kadamb <anikaitkadamb@anikaits-MacBook-Pro.local>
2024-09-27 01:27:42 -0400

    modified polynomial.java and Driver.java

--- Polynomial.java
+++ Polynomial.java
@@ -1,61 +1,83 @@
+
 public class Polynomial{
 
-    double [] polycoeff;
+    double [] coeff;
+    int [] expo;
 
     public Polynomial(){
 
-        this.polycoeff = new double []{0};
+        this.coeff = new double []{0};
+        this.expo = new int []{0};
 
     }
 
-    public Polynomial(double[] array){
+    public Polynomial(double[] coeffarray, int[] expoarray){
 
-        this.polycoeff = array;
+        this.coeff = coeffarray;
+        this.expo = expoarray;
 
     }
 
-    public Polynomial add( Polynomial obj2){
+
+    public int highestIndex(){
+        int highest = 0;
+        int len = this.expo.length;
+        for(int x = 0; x < len; x++){
+            if(highest <= this.expo[x]) highest = this.expo[x];
+        }
+        return highest;
+    }
+
+    public double exists(int exp){
+        int len = this.expo.length;
+        for(int x = 0; x < len; x++){
+            if(this.expo[x] == exp) return this.coeff[x];
+        }
+        return -1;
+    }
+
+    public Polynomial add(Polynomial obj2){
 
         Polynomial objnew = new Polynomial();
-        int len = 0;
-        if(obj2.polycoeff.length > this.polycoeff.length) len = obj2.polycoeff.length;
-        else len = this.polycoeff.length;
-
-        objnew.polycoeff = new double [len];
-        for(int x = 0; x < len; x++){
-            double ob1 = 0;
-            double ob2 = 0;
-            if(x < this.polycoeff.length){
-                ob1 = this.polycoeff[x];
+        int highest = Math.max(this.highestIndex(),obj2.highestIndex());
+        int longest = 0;
+        if(this.expo.length >= obj2.expo.length) longest = this.expo.length;
+        else longest = obj2.expo.length;
+        objnew.coeff = new double [longest];
+        objnew.expo = new int [longest];
+        double var2 = 0;
+        double var1 = 0;
+        int counter = 0;
+        for(int x=0; x <= highest; x++){
+            if(counter >= longest) break;
+            if(this.exists(x) != -1) var1 = this.exists(x);
+            if(obj2.exists(x) != -1) var2 = obj2.exists(x);
+            if(var1 != -1 || var2 != -1){
+                objnew.coeff[counter] = var1 + var2;
+                objnew.expo[counter] = x;
+                counter++;
             }
-            if(x < obj2.polycoeff.length ){
-                ob2 = obj2.polycoeff[x];
-            }
-            objnew.polycoeff[x] = ob1 + ob2;
-
+            var1 = 0;
+            var2 = 0;
         }
         return objnew;
+
     }
 
     public double evaluate(double x){
-        int power = 0;
-        int len = this.polycoeff.length;
+        int len = this.expo.length;
         double sum = 0;
-        for(int y= 0; y < len; y++){
-            sum += (this.polycoeff[y]*(Math.pow(x,power)));
-            power += 1;
+        for(int y = 0; y < len; y++){
+            sum += coeff[y] * (Math.pow(x,expo[y]));
         }
         return sum;
     }
 
     public boolean hasRoot(double x){
-        int power = 0;
-        int len = this.polycoeff.length;
+        int len = this.expo.length;
         double sum = 0;
-        for(int y= 0; y < len; y++){
-
-            sum += (this.polycoeff[y]*(Math.pow(x,power)));
-            power += 1;
+        for(int y = 0; y < len; y++){
+            sum += coeff[y] * (Math.pow(x,expo[y]));
         }
 
         if(sum == 0) return true;
